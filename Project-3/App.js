edcc035feeb43996a939da3bdc7d8d4a41a59b6e
Fafakofi <sfkbenson@st.ug.edu.gh>
2024-06-01 01:25:21 +0000

    one more text component

--- Project-3/App.js
+++ Project-3/App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Scrollview,
+  Textinput,
+  Stylesheet,
+  Button,
+  FlatList,
+  Image,} from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text style={styles.greetings}>Hello , Devs</Text>
+        <Text style={styles.morale}>1443 tasks today. Good job!</Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF5EE',
   },
+
+  greetings: {
+    color: 'black',
+    marginTop: 30,
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginLeft: 20
+  },
+
+  morale: {
+    marginLeft: 20,
+    fontSize: 10
+  }
 });
+
